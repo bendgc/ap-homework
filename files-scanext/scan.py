6d380b7636829b4bc9b542a2d222dcabb9a5cc44
Benjamin Dougnac <benjamin.dougnac@etu.minesparis.psl.eu>
2022-11-08 22:27:48 +0100

    Ajout_de_scan.py

---
+++ files-scanext/scan.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from datetime import datetime as DateTime
+
+def scanv1(nom,ext) :
+    p = Path('.') #liste les sous-dossiers
+    list = p.glob(f"**/*.{ext}") #liste les fichiers ayant la bonne extension
+
+    for q in list : 
+        size = q.stat().st_size
+        dt = DateTime.fromtimestamp(q.stat().st_mtime)
+        with open(str(q), 'r') as f:
+            line = f.readline().strip('\n')
+        
+        print(f""" File {str(q)}
+        {size} B last modified on {dt}
+        first line : {line}          
+        """)
+
